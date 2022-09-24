09d4916088440652fac82f024f16773c9c1442dd
Metin <metinaydin3442@gmail.com>
2022-09-24 14:19:58 +0300

    son

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-5
+const container = document.querySelector(".container");
+const count = document.querySelector(".count");
+const amount = document.querySelector(".amount");
+const movie = document.getElementById("movie");
+container.addEventListener("click", function(e){
+    if(e.target.classList.contains("seat")&& !e.target.classList.contains("reserved")){
+        e.target.classList.toggle("selected");
+    }
+    let selectedSeatCount = container.querySelectorAll(".seat.selected").length;
+    let price = movie.value;
+    count.innerText = selectedSeatCount;
+    amount.innerText = price*selectedSeatCount;
+    }
+    
+);
+movie.addEventListener("change", function(e){
+    let selectedSeatCount = container.querySelectorAll(".seat.selected").length;
+    let price = movie.value;
+    count.innerText = selectedSeatCount;
+    amount.innerText = price*selectedSeatCount;
+})
+
